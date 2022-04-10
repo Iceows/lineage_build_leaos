40dc1fb9fcc0f190fffd297ff5bffb0d64c499e6
Raphael Mounier <mounierr07@gmail.com>
2022-04-10 16:30:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# lineage_build_leaos
- Script to build LeaOS
+
+## Building PHH-based LineageOS GSIs ##
+
+To get started with building LineageOS GSI, you'll need to get familiar with [Git and Repo](https://source.android.com/source/using-repo.html), and set up your environment by referring to [LineageOS Wiki](https://wiki.lineageos.org/devices/redfin/build) (mainly "Install the build packages") and [How to build a GSI](https://github.com/phhusson/treble_experimentations/wiki/How-to-build-a-GSI%3F).
+
+
+First, open a new Terminal window, create a new working directory for your LineageOS build (leaos for example) and navigate to it:
+
+    mkdir leaos; cd leaos
+    
+Clone the modified treble_experimentations repo there:
+
+    git clone https://github.com/iceows/treble_experimentations
+
+Initialize your LineageOS workspace:
+
+    repo init -u https://github.com/LineageOS/android.git -b lineage-18.1
+
+Clone both this and the patches repos:
+
+    git clone https://github.com/iceows/lineage_build_unified lineage_build_unified -b lineage-18.1
+    git clone https://github.com/iceows/lineage_patches_unified lineage_patches_unified -b lineage-18.1
+
+Finally, start the build script (Dynamic root):
+
+    bash lineage_build_unified/buildbot_unified.sh treble 64BZ
+    
+or to include AndyCG patch
+    bash lineage_build_unified/buildbot_unified.sh treble personal iceows 64BZ
+
+or to include also Iceows patch
+    bash lineage_build_unified/buildbot_unified.sh treble personal iceows 64BZ
+
+Be sure to update the cloned repos from time to time!
+
+---
+
+A-only targets for Huawei hi6250 re generated from AB images instead of source-built - refer to [huawei-creator](https://github.com/iceows/huawei-creator).
+
+	sudo ./run-huawei-aonly.sh "myimage.img"  "LeaOS" 
+
+---
+
+This script is also used to make builds without sync repo. To do so add nosync in the command build line.
+
+    bash lineage_build_unified/buildbot_unified.sh treble nosync personal iceows 64BZ
+
+
---
+++ buildbot_unified.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+echo ""
+echo "LineageOS 18.x Unified Buildbot - Iceows version"
+echo "Executing in 5 seconds - CTRL-C to exit"
+echo ""
+sleep 5
+
+if [ $# -lt 2 ]
+then
+    echo "Not enough arguments - exiting"
+    echo ""
+    exit 1
+fi
+
+MODE=${1}
+if [ ${MODE} != "device" ] && [ ${MODE} != "treble" ]
+then
+    echo "Invalid mode - exiting"
+    echo ""
+    exit 1
+fi
+
+NOSYNC=false
+PERSONAL=false
+ICEOWS=false
+for var in "${@:2}"
+do
+    if [ ${var} == "nosync" ]
+    then
+        NOSYNC=true
+    fi
+    if [ ${var} == "personal" ]
+    then
+        PERSONAL=true
+    fi
+    if [ ${var} == "iceows" ]
+    then
+        ICEOWS=true
+    fi    
+done
+
+echo "Building with NoSync : $NOSYNC - Personal patch : $PERSONAL - Iceows patch : $ICEOWS - Mode : ${MODE}"
+
+
+
+# Abort early on error
+set -eE
+trap '(\
+echo;\
+echo \!\!\! An error happened during script execution;\
+echo \!\!\! Please check console output for bad sync,;\
+echo \!\!\! failed patch application, etc.;\
+echo\
+)' ERR
+
+START=`date +%s`
+BUILD_DATE="$(date +%Y%m%d)"
+WITHOUT_CHECK_API=true
+WITH_SU=true
+
+prep_build() {
+	echo "Preparing local manifests"
+	mkdir -p .repo/local_manifests
+	cp ./lineage_build_leaos/local_manifests_leaos/*.xml .repo/local_manifests
+	echo ""
+
+	echo "Syncing repos"
+	repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+	echo ""
+
+	echo "Setting up build environment"
+	source build/envsetup.sh &> /dev/null
+	mkdir -p ~/build-output
+	echo ""
+}
+
+apply_patches() {
+    echo "Applying patch group ${1}"
+    bash ./treble_experimentations/apply-patches.sh ./lineage_patches_leaos/${1}
+}
+
+prep_device() {
+    :
+}
+
+prep_treble() {
+    echo "Applying patch treble prerequisite and phh"
+
+}
+
+finalize_device() {
+    :
+}
+
+finalize_treble() {
+    rm -f device/*/sepolicy/common/private/genfs_contexts
+    cd device/phh/treble
+    git clean -fdx
+    bash generate.sh lineage
+    cd ../../..
+}
+
+build_device() {
+    if [ ${1} == "arm64" ]
+    then
+        lunch lineage_arm64-userdebug
+        make -j$(nproc --all) systemimage
+        mv $OUT/system.img ~/build-output/lineage-18.1-$BUILD_DATE-UNOFFICIAL-arm64$(${PERSONAL} && echo "-personal" || echo "").img
+    else
+        brunch ${1}
+        mv $OUT/lineage-*.zip ~/build-output/lineage-18.1-$BUILD_DATE-UNOFFICIAL-${1}$($PERSONAL && echo "-personal" || echo "").zip
+    fi
+}
+
+build_treble() {
+    case "${1}" in
+        ("32B") TARGET=treble_arm_bvS;;
+        ("32BZ") TARGET=treble_arm_bvZ;;
+        ("32BN") TARGET=treble_arm_bvN;;
+        ("A64B") TARGET=treble_a64_bvS;;
+        ("A64BZ") TARGET=treble_a64_bvZ;;
+        ("A64BN") TARGET=treble_a64_bvN;;
+        ("64B") TARGET=treble_arm64_bvS;;
+        ("64BZ") TARGET=treble_arm64_bvZ;;
+        ("64BN") TARGET=treble_arm64_bvN;;
+        (*) echo "Invalid target - exiting"; exit 1;;
+    esac
+    lunch ${TARGET}-userdebug
+    make installclean
+    make -j$(nproc --all) systemimage
+    make vndk-test-sepolicy
+    mv $OUT/system.img ~/build-output/lineage-18.1-$BUILD_DATE-UNOFFICIAL-${TARGET}$(${PERSONAL} && echo "-personal" || echo "").img
+}
+
+if ${NOSYNC}
+then
+    echo "ATTENTION: syncing/patching skipped!"
+    echo ""
+    echo "Setting up build environment"
+    source build/envsetup.sh &> /dev/null
+    echo ""
+else
+    prep_build
+    echo "Applying patches"
+    prep_treble
+    apply_patches lineage
+    apply_patches generic
+    apply_patches personal
+    finalize_treble
+    echo ""
+fi
+
+for var in "${@:2}"
+do
+    if [ ${var} == "nosync" ] || [ ${var} == "personal" ]  || [ ${var} == "iceows" ]
+    then
+        continue
+    fi
+    echo "Starting $(${PERSONAL} && echo "personal " || echo "")build for ${MODE} ${var}"
+    build_${MODE} ${var}
+done
+ls ~/build-output | grep 'lineage' || true
+
+END=`date +%s`
+ELAPSEDM=$(($(($END-$START))/60))
+ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
+echo "Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
+echo ""
