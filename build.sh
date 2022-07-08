179f1181b6455e13cf7f05adbab5218c0679f1c2
Raphael Mounier <mounierr07@gmail.com>
2022-07-08 21:35:16 +0200

    Add lineage 19.1 script

--- build.sh
+++ build.sh
@@ -46,21 +46,32 @@ WITH_SU=true
 repo init -u https://github.com/LineageOS/android.git -b lineage-18.1
 
 prep_build() {
-	echo "Preparing local manifests"
-	rm -rf .repo/local_manifests
-	mkdir -p .repo/local_manifests
-	cp ./lineage_build_leaos/local_manifests_leaos/*.xml .repo/local_manifests
-	echo ""
+    echo "Preparing local manifests"
+    mkdir -p .repo/local_manifests
+    cp ./lineage_build_leaos/local_manifests_leaos/*.xml .repo/local_manifests
+    echo ""
 
-	echo "Syncing repos"
-	repo sync -j$(nproc --all) -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune
+    echo "Syncing repos"
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+    echo ""
 
-	echo ""
+    echo "Setting up build environment"
+    source build/envsetup.sh &> /dev/null
+    mkdir -p ~/build-output
+    echo ""
 
-	echo "Setting up build environment"
-	source build/envsetup.sh &> /dev/null
-	mkdir -p ~/build-output
-	echo ""
+    repopick -Q "(status:open+AND+NOT+is:wip)+(label:Code-Review>=0+AND+label:Verified>=0)+project:LineageOS/android_packages_apps_Trebuchet+branch:lineage-19.1+NOT+332083"
+    repopick -t twelve-burnin
+    repopick -t qs-lightmode
+    repopick -t powermenu-lightmode
+    repopick 321337 # Deprioritize important developer notifications
+    repopick 321338 # Allow disabling important developer notifications
+    repopick 321339 # Allow disabling USB notifications
+    repopick 329229 -f # Alter model name to avoid SafetyNet HW attestation enforcement
+    repopick 329230 -f # keystore: Block key attestation for SafetyNet
+    repopick 329409 # SystemUI: screenshot: open the screenshot instead of edit
+    repopick 331534 -f # SystemUI: Add support to add/remove QS tiles with one tap
+    repopick 331791 # Skip checking SystemUI's permission for observing sensor privacy
 }
 
 apply_patches() {
