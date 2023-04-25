dafba5129c96ef48f667c7d30da49cce8482173a
Raphael Mounier <mounierr07@gmail.com>
2023-04-25 08:11:35 +0200

    Build r41 TD

--- build.sh
+++ build.sh
@@ -71,20 +71,27 @@ prep_build() {
     repopick 321337 -f # Deprioritize important developer notifications
     repopick 321338 -f # Allow disabling important developer notifications
     repopick 321339 -f # Allow disabling USB notifications
-     #repopick 340916 # SystemUI: add burnIn protection
+    repopick 340916 # SystemUI: add burnIn protection
+    repopick 342860 # codec2: Use numClientBuffers to control the pipeline
+    repopick 342861 # CCodec: Control the inputs to avoid pipeline overflow
+    repopick 342862 # [WA] Codec2: queue a empty work to HAL to wake up allocation thread
+    repopick 342863 # CCodec: Use pipelineRoom only for HW decoder
+    repopick 342864 # codec2: Change a Info print into Verbose
 
 
+    # Only if you hae not include in the repo init --git-lfs
     # Walk the entire tree starting at $PWD and find all directories
     # containing `.gitattributes` file that says it's a Git clone
     # directory that uses Git-LFS. For each such directory, do `git lfs
     # pull`.
-    cd external
-    grep -l 'merge=lfs' $( find $PWD -name .gitattributes ) /dev/null | while IFS= read -r line; do
-       dir=$(dirname $line)
-       echo $dir
-       ( cd $dir ; git lfs pull )
-    done
-    cd ..
+    
+    #cd external
+    #grep -l 'merge=lfs' $( find $PWD -name .gitattributes ) /dev/null | while IFS= read -r line; do
+    #   dir=$(dirname $line)
+    #   echo $dir
+    #   ( cd $dir ; git lfs pull )
+    #done
+    #cd ..
 
 }
 
