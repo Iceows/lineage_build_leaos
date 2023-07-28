444d0cbae09d646dd12199294647fbeb03873c49
Raphael Mounier <mounierr07@gmail.com>
2023-07-28 20:35:08 +0200

    add device prague

--- clone_hi6250-8_s_buildbots.sh
+++ clone_hi6250-8_s_buildbots.sh
@@ -11,18 +11,19 @@
 BRANCH="lineage-20"
 LOCAL_PATH="."
 GITHUB="https://github.com/"
-
+GITLAB="https://gitlab.com/"
 
 # Repos
 DEVICE_COMMON="hisi-oss/android_device_huawei_hi6250-8-common"
 DEVICE_COMMON_PATH="device/huawei/hi6250-8-common"
-VENDOR_COMMON="hisi-oss/proprietary_vendor_huawei_hi6250-8-common"
-VENDOR_COMMON_PATH="vendor/huawei/hi6250-8-common"
+POLICY="hisi-oss/android_device_hisi_sepolicy"
+POLICY_PATH="device/hisi/sepolicy"
 KERNEL="hisi-oss/android_kernel_huawei_hi6250-8"
 KERNEL_PATH="kernel/huawei/hi6250-8"
 HARDWARE="hisi-oss/android_hardware_hisi"
 HARDWARE_PATH="hardware/hisi"
 
+# CLANG for Kernel
 CLANG="iceows/android_prebuilts_clang_host_linux-x86_clang-r416183b1"
 CLANG_PATH="prebuilts/clang/host/linux-x86/clang-r416183b1"
 
@@ -32,24 +33,37 @@ DEVICE_WARSAW_PATH="device/huawei/warsaw"
 VENDOR_WARSAW="hisi-oss/proprietary_vendor_huawei_warsaw"
 VENDOR_WARSAW_PATH="vendor/huawei/warsaw"
 
+# Huawei Prague repo
+DEVICE_PRAGUE="hisi-oss/android_device_huawei_prague"
+DEVICE_PRAGUE_PATH="device/huawei/prague"
+
+# EMUI8 common and prague (gitlab)
+VENDOR_COMMON_DEVICES="itsvixano-dev/proprietary_vendor_huawei.git"
+VENDOR_HUAWEI_PATH="vendor/huawei/"
+
 
 # Remove all
 rm -rf "$DEVICE_COMMON_PATH"
-rm -rf "$VENDOR_COMMON_PATH" 
+rm -rf "$VENDOR_HUAWEI_PATH/prague"
+rm -rf "$VENDOR_HUAWEI_PATH/hi6250-8-common" 
 rm -rf "$KERNEL_PATH"
 rm -rf "$HARDWARE_PATH"
 rm -rf "$DEVICE_WARSAW_PATH"
-rm -rf "$VENDOR_WARSAW_PATH"
+rm -rf "$DEVICE_PRAGUE_PATH"
+rm -rf "$POLICY_PATH"
+
 
 
 
 # Clone Repos
 git clone "$GITHUB""$DEVICE_COMMON" "$DEVICE_COMMON_PATH" -b "$BRANCH"
 git clone "$GITHUB""$DEVICE_WARSAW" "$DEVICE_WARSAW_PATH" -b "$BRANCH"
-git clone "$GITHUB""$VENDOR_COMMON" "$VENDOR_COMMON_PATH"
+git clone "$GITHUB""$DEVICE_PRAGUE" "$DEVICE_PRAGUE_PATH" -b "$BRANCH"
+git clone "$GITLAB""$VENDOR_COMMON_DEVICES" "$VENDOR_HUAWEI_PATH" -b "$BRANCH"
 git clone "$GITHUB""$VENDOR_WARSAW" "$VENDOR_WARSAW_PATH" -b "$BRANCH"
 git clone "$GITHUB""$KERNEL" "$KERNEL_PATH" -b "$BRANCH"
 git clone "$GITHUB""$HARDWARE" "$HARDWARE_PATH" -b "$BRANCH"
+git clone "$GITHUB""$POLICY" "$POLICY_PATH" -b "$BRANCH"
 
 git clone "$GITHUB""$CLANG" "$CLANG_PATH"
 
