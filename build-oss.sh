299b2255dd41c1db76275ccec6c3247f5e9daefd
Raphael Mounier <mounierr07@gmail.com>
2023-12-29 09:01:57 +0100

    build: update build-oss to include apply_patches

--- build-oss.sh
+++ build-oss.sh
@@ -3,6 +3,40 @@ echo ""
 echo "LineageOS 20.x - LeaOS-OSS version"
 echo ""
 
+if [ $# -lt 1 ]
+then
+    echo "Not enough arguments - exiting"
+    echo ""
+    exit 1
+fi
+
+
+PERSONAL=false
+ICEOWS=true
+
+MODE=${1}
+if [ ${MODE} != "device" ] && [ ${MODE} != "treble" ]
+then
+    echo "Invalid mode - exiting"
+    echo ""
+    exit 1
+fi
+
+NOSYNC=false
+for var in "${@:2}"
+do
+    if [ ${var} == "nosync" ]
+    then
+        NOSYNC=true
+    fi
+done
+
+
+echo "Building with NoSync : $NOSYNC - Mode : ${MODE}"
+
+echo "Syncing repos"
+repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+echo ""
 
 echo ""
 echo "Setting up build environment"
@@ -24,8 +58,22 @@ echo "Unzip blob"
 unzip -o ./vendor/huawei/hi6250-9-common/proprietary/vendor/firmware/isp_dts.zip -d ./vendor/huawei/hi6250-9-common/proprietary/vendor/firmware
 unzip -o ./vendor/huawei/hi6250-8-common/proprietary/vendor/firmware/isp_dts.zip -d ./vendor/huawei/hi6250-8-common/proprietary/vendor/firmware
 
+echo "Apply Iceows patches for Huawei device"
+bash ./apply_patches.sh ./lineage_patches_leaos/patches_device
+bash ./apply_patches.sh ./lineage_patches_leaos/patches_device_iceows
+
+
+    	
 echo "Start compile"
-brunch anne
+for var in "${@:2}"
+do
+    if [ ${var} == "nosync" ]
+    then
+        continue
+    fi
+    echo "Starting " || echo " build for ${MODE} ${var}"
+    brunch ${var}
+done
 
 echo ""
 
