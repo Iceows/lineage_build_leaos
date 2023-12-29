86f40efb4cafe5a346af5a5f10a04ccdc66495cf
Raphael Mounier <mounierr07@gmail.com>
2023-12-29 04:43:07 +0100

    build: add build oss script

--- build-oss.sh
+++ build-oss.sh
@@ -1,66 +1,31 @@
 #!/bin/bash
 echo ""
-echo "LineageOS 20.x Unified Buildbot - LeaOS-OSS version"
-echo "Executing in 5 seconds - CTRL-C to exit"
+echo "LineageOS 20.x - LeaOS-OSS version"
 echo ""
-sleep 5
-
-if [ $# -lt 1 ]
-then
-    echo "Not enough arguments - exiting"
-    echo ""
-    exit 1
-fi
 
 
-PERSONAL=false
-ICEOWS=true
+echo ""
+echo "Setting up build environment"
+source build/envsetup.sh
+echo ""
 
-MODE=${1}
-if [ ${MODE} != "device" ] && [ ${MODE} != "treble" ]
-then
-    echo "Invalid mode - exiting"
-    echo ""
-    exit 1
-fi
+echo "Pick patch"
+repopick 321337 -f # Deprioritize important developer notifications
+repopick 321338 -f # Allow disabling important developer notifications
+repopick 321339 -f # Allow disabling USB notifications
+repopick 340916 # SystemUI: add burnIn protection
+repopick 342860 # codec2: Use numClientBuffers to control the pipeline
+repopick 342861 # CCodec: Control the inputs to avoid pipeline overflow
+repopick 342862 # [WA] Codec2: queue a empty work to HAL to wake up allocation thread
+repopick 342863 # CCodec: Use pipelineRoom only for HW decoder
+repopick 342864 # codec2: Change a Info print into Verbose
 
-NOSYNC=false
-for var in "${@:2}"
-do
-    if [ ${var} == "nosync" ]
-    then
-        NOSYNC=true
-    fi
-done
+echo "Unzip blob"
+unzip -o ./vendor/huawei/hi6250-9-common/proprietary/vendor/firmware/isp_dts.zip -d ./vendor/huawei/hi6250-9-common/proprietary/vendor/firmware
+unzip -o ./vendor/huawei/hi6250-8-common/proprietary/vendor/firmware/isp_dts.zip -d ./vendor/huawei/hi6250-8-common/proprietary/vendor/firmware
 
-echo "Building with NoSync : $NOSYNC - Mode : ${MODE}"
+echo "Start compile"
+brunch anne
 
-# Abort early on error
-set -eE
-trap '(\
-echo;\
-echo \!\!\! An error happened during script execution;\
-echo \!\!\! Please check console output for bad sync,;\
-echo \!\!\! failed patch application, etc.;\
-echo\
-)' ERR
-
-
-WITHOUT_CHECK_API=true
-WITH_SU=true
-START=`date +%s`
-BUILD_DATE="$(date +%Y%m%d)"
-
-export OUT_DIR=/home/iceows/build/LeaOS
-
-
-
-
-
-
-END=`date +%s`
-ELAPSEDM=$(($(($END-$START))/60))
-ELAPSEDS=$(($(($END-$START))-$ELAPSEDM*60))
-echo "Buildbot completed in $ELAPSEDM minutes and $ELAPSEDS seconds"
 echo ""
 
