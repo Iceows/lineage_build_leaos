e105b6dfdfc98dd76a94b03f1e76d9c50a8cf05d
Raphael Mounier <mounierr07@gmail.com>
2024-03-24 14:54:48 +0100

    build: remove some repopick for ccodec

--- build.sh
+++ build.sh
@@ -79,17 +79,13 @@ prep_build() {
 
     if [ ${MODE} == "device" ]
     then
-      echo "no repo pick for device"
+	echo "no repo pick for device"
     else
-      repopick 321337 -f # Deprioritize important developer notifications
-      repopick 321338 -f # Allow disabling important developer notifications
-      repopick 321339 -f # Allow disabling USB notifications
-      repopick 340916 # SystemUI: add burnIn protection
-      repopick 342860 # codec2: Use numClientBuffers to control the pipeline
-      repopick 342861 # CCodec: Control the inputs to avoid pipeline overflow
-      repopick 342862 # [WA] Codec2: queue a empty work to HAL to wake up allocation thread
-      repopick 342863 # CCodec: Use pipelineRoom only for HW decoder
-      repopick 342864 # codec2: Change a Info print into Verbose      
+	repopick -t 13-taro-kalama -r -f
+	repopick 321337 -r -f # Deprioritize important developer notifications
+	repopick 321338 -r -f # Allow disabling important developer notifications
+	repopick 321339 -r -f # Allow disabling USB notifications
+	repopick 340916 -r # SystemUI: add burnIn protection     
     fi
 
 }
